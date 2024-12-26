c7a3c76627722b129f6f5498c34b4cc0d391541d
idksarah <sarah.p.ngai@gmail.com>
2024-12-26 15:49:08 -0800

    2nd blockGame

--- java/usaco/blockGame.java
+++ java/usaco/blockGame.java
@@ -1,4 +1,6 @@
 //1:00 pm 12/26/26
+//1st run 3:42
+//2nd 3:48
 
 import java.io.*;
 import java.util.Scanner;
@@ -8,38 +10,21 @@ public class blockGame{
 		Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] overallCount = new int[26];
-        char[] abcs = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-        for(int i =0; i< N; i++){ //check each word pair
+        for(int i =0; i< N; i++){ 
             int[] tempCountFront = new int[26];
             int[] tempCountBack = new int[26];
             String front = sc.next();
             String back = sc.next();
-            for(int a=0; a<front.length(); a++){ //check each letter of front
-                for(int b =0; b<26;b++){ //check each letter of abc
-                    if(front.charAt(a) == abcs[b]){
-                        tempCountFront[b]++;
-                    }
-                }
+            for(int a=0; a<front.length(); a++){
+                tempCountFront[front.charAt(a) -'a']++; //get current character, convert to int, use as index for tempCountFront, and increment value
             }
-            // System.out.println("front letters" + Arrays.toString(tempCountFront));
-            for(int a=0; a<back.length(); a++){ //check each letter of back
-                for(int b =0; b<26;b++){ //check each letter of abc
-                    if(back.charAt(a) == abcs[b]){
-                        tempCountBack[b]++;
-                    }
-                }
-            }
-            // System.out.println("back letters" + Arrays.toString(tempCountBack));
-            
+            for(int a=0; a<back.length(); a++){ 
+                tempCountBack[back.charAt(a) -'a']++;
+            }            
             for(int c =0; c< 26; c++){ //for each letter
-                if(tempCountFront[c] > tempCountBack[c] || tempCountFront[c] == tempCountBack[c]){
-                    overallCount[c] += tempCountFront[c];
-                } else if(tempCountFront[c] < tempCountBack[c]){
-                    overallCount[c] += tempCountBack[c];
-                }
+                overallCount[c] += Math.max(tempCountFront[c], tempCountBack[c]);
             }
-            // System.out.println(Arrays.toString(overallCount));
         }
         for(int i=0; i<26;i++){
             System.out.println(overallCount[i]);
