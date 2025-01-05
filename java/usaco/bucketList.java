d12652bf953cb23a36897ca7fa94a9b2e2a61faf
idksarah <sarah.p.ngai@gmail.com>
2025-01-05 15:56:16 -0800

    lost cow kinda works

--- java/usaco/bucketList.java
+++ java/usaco/bucketList.java
@@ -1,69 +1,55 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 //2:06 pm 1/1/2025 !!!
 //fml 3:05
+//passes test case 4:33 1/3/25
+//break at 5
+//back at 5:42
 public class bucketList {
     public static void main(String[] args) throws IOException{
-        // BufferedReader br = new BufferedReader(new FileReader("blist.in"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // PrintWriter pw = new PrintWriter("blist.out");
-        PrintWriter pw = new PrintWriter(System.out);
+        BufferedReader br = new BufferedReader(new FileReader("blist.in"));
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter("blist.out");
+        // PrintWriter pw = new PrintWriter(System.out);
 
         int N = Integer.parseInt(br.readLine());
 
-        int[][] everything = new int[N][3];
-        int freeBuckets=0, numOfBuckets=0, startCount=0, endCount=0, bucketCount=0;
+        int[] starts = new int[N], ends = new int[N], amts = new int[N];
 
         for(int i = 0; i < N; i++){
             String[] temp = br.readLine().split(" ");
-            everything[i][0] = Integer.parseInt(temp[0]);
-            everything[i][1] = Integer.parseInt(temp[1]);
-            everything[i][2] = Integer.parseInt(temp[2]);
+            starts[i] = Integer.parseInt(temp[0]);
+            ends[i] = Integer.parseInt(temp[1]);
+            amts[i] = Integer.parseInt(temp[2]);
         }
 
-        // System.out.println(Arrays.toString(starts));
-        // System.out.println(Arrays.toString(ends));
-        // System.out.println(Arrays.toString(buckets));
+        int[] sortedEnds = Arrays.copyOf(ends, ends.length);
+        Arrays.sort(sortedEnds);
+        int[] events = new int[sortedEnds[N-1]];
 
-        // int[] timeline = new int[ends[N-1]];
-        // for(int i =0; i < ends[N-1]; i++){
-        //     timeline[i] = i+1;
-        // }
-
-        // System.out.println(Arrays.toString(timeline));
-
-        
-        System.out.println(Arrays.toString(buckets));
-
-        for(int a =1; a < ends[N-1]+1; a ++){
-            System.out.println("time:" + a);
-            if(startCount != N && a == starts[startCount]){
-                // System.out.println("start" + starts[startCount]);
-                if(buckets[bucketCount] < freeBuckets){
-                    // System.out.println("less buckets required than free");
-                    freeBuckets-= buckets[bucketCount];
-                    bucketCount++;
-                } else {
-                    // System.out.println("more buckets required than free");
-                    // System.out.println("buckets immediately needed" + buckets[bucketCount]);
-                    numOfBuckets += (buckets[bucketCount] - freeBuckets);
-                    freeBuckets=0;
-                    bucketCount++;
-                }
-                // System.out.println("buckets needed" + numOfBuckets);
-                // System.out.println("bucket count" + bucketCount);
-                // System.out.println("start count" + startCount);
-                // System.out.println("free buckets" + freeBuckets);
-                startCount++;
-            }else if(endCount != N && a == ends[endCount]){
-                freeBuckets += buckets[endCount];
-                // System.out.println("free bucket count" + freeBuckets);
-                endCount++;
+        for(int a = 0; a < N; a ++){
+            int start = starts[a];
+            int end = ends[a];
+            int amt = amts[a];
+            for(int b =start-1; b < end; b++){
+                events[b] += amt;
             }
         }
 
-        System.out.println(numOfBuckets);
+        // System.out.println(Arrays.toString(events));
+
+        int max=0;
+
+        for(int i =0; i < sortedEnds[N-1]; i ++){
+            if(events[i] > max){
+                max = events[i];
+            }
+        }
+
+        // System.out.println(max);
+        pw.println(max);
+        pw.close();
         
     }
 }
--- java/usaco/lostCow.java
+++ java/usaco/lostCow.java
@@ -1,9 +1,55 @@
 import java.io.*;
 
+//3:28 pm 1/5/2025
+//YOOOOO passes first 3:54
+
 public class lostCow {
     public static void main(String[] args) throws IOException{
-        // BufferedReader br = new BufferedReader(new FileReader("lostcow.in"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
+        BufferedReader br = new BufferedReader(new FileReader("lostcow.in"));
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter("lostcow.out");
+
+        String temp[] = br.readLine().split(" ");
+        int start = Integer.parseInt(temp[0]);
+        int bessie = Integer.parseInt(temp[1]);
+        int current = start;
+
+        boolean right = true;
+        boolean johnnyLeft;
+        int nextMove = 1, distance=0;
+
+        if(start < bessie){
+            johnnyLeft=true;
+        } else {
+            johnnyLeft = false;
+        }
+        while(current != bessie){
+            if(johnnyLeft){ //if john is left of bessie    
+                if(right){
+                    if((current + nextMove) > bessie){
+                        distance += bessie-start;
+                        current = bessie;
+                    } else {
+                        distance += 2 * (nextMove);
+                    }
+                } else {
+                    distance += 2 * (nextMove);
+                }
+            } else {
+                if(!right){
+                    if((current + nextMove) < bessie){
+                        distance += bessie-start;
+                        current = bessie;
+                    } else {
+                        distance += 2 * (nextMove);
+                    }
+                } else {
+                    distance += 2 * (nextMove);
+                }
+            }
+            nextMove*=2;
+        }
+        pw.println(distance);
+        pw.close();
     }
 }
