785ff6d5a399d2b8efde0ad4b43c0b6b6aec403e
idksarah <sarah.p.ngai@gmail.com>
2025-01-05 16:45:13 -0800

    commit

---
+++ java/codeforces/playingInACasino.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+// 6:18 pm 1/3/2025
+
+public class playingInACasino{
+    public static void main(String[] args) throws IOException{
+        
+    }
+}
--- java/usaco/lostCow.java
+++ java/usaco/lostCow.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 //3:28 pm 1/5/2025
 //YOOOOO passes first 3:54
+// 4 : 17 :33333
 
 public class lostCow {
     public static void main(String[] args) throws IOException{
@@ -10,45 +11,46 @@ public class lostCow {
         PrintWriter pw = new PrintWriter("lostcow.out");
 
         String temp[] = br.readLine().split(" ");
-        int start = Integer.parseInt(temp[0]);
+        int current = Integer.parseInt(temp[0]);
         int bessie = Integer.parseInt(temp[1]);
-        int current = start;
 
         boolean right = true;
-        boolean johnnyLeft;
         int nextMove = 1, distance=0;
 
-        if(start < bessie){
-            johnnyLeft=true;
-        } else {
-            johnnyLeft = false;
-        }
         while(current != bessie){
-            if(johnnyLeft){ //if john is left of bessie    
+            // System.out.println("right" + right);
+            if(current < bessie){ //if john is left of bessie    
                 if(right){
-                    if((current + nextMove) > bessie){
-                        distance += bessie-start;
+                    if((current + nextMove) >= bessie){
+                        distance += Math.abs(bessie-current);
                         current = bessie;
                     } else {
                         distance += 2 * (nextMove);
                     }
+                    right = false;
                 } else {
                     distance += 2 * (nextMove);
+                    right = true;
                 }
             } else {
                 if(!right){
-                    if((current + nextMove) < bessie){
-                        distance += bessie-start;
+                    if((current - nextMove) <= bessie){
+                        distance += Math.abs(bessie-current);
                         current = bessie;
                     } else {
                         distance += 2 * (nextMove);
                     }
+                    right = true;
                 } else {
                     distance += 2 * (nextMove);
+                    right = false;
                 }
             }
             nextMove*=2;
+            // System.out.println("distance" + distance);
+            // System.out.println("next" + nextMove);
         }
+        // System.out.println(distance);
         pw.println(distance);
         pw.close();
     }
