d9e234f17a5e926f992720cbc559d8c6709d79dc
idksarah <sarah.p.ngai@gmail.com>
2025-01-07 23:00:48 -0800

    commit

--- java/usaco/measuringTraffic.java
+++ java/usaco/measuringTraffic.java
@@ -1,5 +1,9 @@
 import java.io.*;
 // 12:15am 1/7/25
+//9:27 pm 1/7/25
+//passes sample 10:24
+
+//passes 2 cases......
 public class measuringTraffic {    
     public static void main(String[] args) throws IOException{
         // BufferedReader br = new BufferedReader(new FileReader("traffic.in"));
@@ -17,17 +21,50 @@ public class measuringTraffic {
             segments[i][2] = temp[2];
         }
 
-        int prior[] = new int[2], post[] = new int[2];
-        for(int a =0; a < N; a ++){
-            if(segments[a][0] == "on"){
-                
-            } else if(segments[a][0] == "none"){
+        // System.out.println(Arrays.deepToString(segments));
 
+        int current[] = new int[2];
+        current[1] = 1001;
+        
+        for(int a =N-1; a > -1; a--){ // prior
+            if(segments[a][0].equals("on")){
+                // System.out.println("on");
+                current[0] -= Integer.parseInt(segments[a][2]);
+                current[1] -= Integer.parseInt(segments[a][1]);
+            } else if(segments[a][0].equals("none")){
+                // System.out.println("none");
+                current[0] = Math.max(Integer.parseInt(segments[a][1]), current[0]);
+                current[1] = Math.min(Integer.parseInt(segments[a][2]), current[1]);
             } else {
-
+                // System.out.println("off");
+                current[0] += Integer.parseInt(segments[a][2]);
+                current[1] += Integer.parseInt(segments[a][1]);
+            }
+            // System.out.println(Arrays.toString(current));
+        }
+        // System.out.println(Arrays.toString(current));
+        pw.print(current[0] + " ");
+        pw.println(current[1]);
+        current[0] = 0;
+        current[1] = 1001;
+        for(int a =0; a < N; a ++){ // post
+            if(segments[a][0].equals("on")){
+                // System.out.println("on");
+                current[0] += Integer.parseInt(segments[a][2]);
+                current[1] += Integer.parseInt(segments[a][1]);
+            } else if(segments[a][0].equals("none")){
+                // System.out.println("none");
+                current[0] = Math.max(Integer.parseInt(segments[a][1]), current[0]);
+                current[1] = Math.min(Integer.parseInt(segments[a][2]), current[1]);
+            } else {
+                // System.out.println("off");
+                current[0] -= Integer.parseInt(segments[a][2]);
+                current[1] -= Integer.parseInt(segments[a][1]);
             }
         }
-
-        pw.close();
+        // System.out.println(Arrays.toString(current));
+        // pw.print(current[0] + " ");
+        // pw.println(current[1]);
+        // pw.close();
     }
 }
