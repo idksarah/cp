8d6f489b61e1c213d41e76a92f8b09847d4e22b4
idksarah <sarah.p.ngai@gmail.com>
2025-01-18 14:48:38 -0800

    mooooowin the field

--- java/usaco/mowingTheField.java
+++ java/usaco/mowingTheField.java
@@ -4,36 +4,74 @@ import java.util.*;
 
 // 1:17 am 1/17/25!!
 // sleepe 1:49
+// 1:17 pm 1/18/25
+// first passed 2:42;; passed 7 cases :(
+// passes all 2:48 :3
 
 public class mowingTheField {
     public static void main(String[] args) throws IOException{
-        // BufferedReader br = new BufferedReader(FileReader("mowing.in"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // PrintWriter pw = new PrintWriter("mowing.out");
+        BufferedReader br = new BufferedReader(new FileReader("mowing.in"));
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter("mowing.out");
 
-        ArrayList points = new ArrayList();
         int N = Integer.parseInt(br.readLine());
-        int x=0, y=0;
-        for(int i =0; i < N;i ++){
-            String[] line = br.readLine().split(" ");
-            char direction = line[0].charAt(0);
-            int steps = Integer.parseInt(line[1]);
-            if(direction=='N'){
-                points.add(new int[]{x,steps});
-                y+=steps;
-            } else if(direction=='E'){
-                points.add(new int[]{steps,y});
-                x+=steps;
-            }else if(direction=='S'){
-                points.add(new int[]{x,-steps});
-                y+=steps;
-            } else {
-                points.add(new int[]{-steps,y});
-                x-=steps;
-            }
+        int x=0, y=0, time=1, max=Integer.MAX_VALUE, statement=0;
+        Map<String, Integer> visited = new HashMap<>();
+        visited.put(0 +" "+0, 0);
+        String[] directions = new String[2*N];
+        for(int i =0; i < N*2;i+=2){
+            String temp[] = br.readLine().split(" ");
+            directions[i] = temp[0];
+            directions[i+1] = temp[1];
         }
-        // for(Object point:points){
-        //     System.out.println(Arrays.toString((int[])point));
-        // }
+        while(statement < N*2){
+            String direction = directions[statement];
+            int steps=Integer.parseInt(directions[statement+1]);
+            if(direction.equals("N")){
+                for(int i=0; i < steps;i++){
+                    y+=1;
+                    if(visited.containsKey(x+" "+y)){
+                        max = Math.min(max, time-visited.get(x+" "+y));
+                    }
+                    visited.put((x+" "+y), time);
+                    time++;
+                }
+            } else if(direction.equals("E")){
+                for(int i=0; i < steps;i++){
+                    x+=1;
+                    if(visited.containsKey(x+" "+y)){
+                        max = Math.min(max, time-visited.get(x+" "+y));
+                    }
+                    visited.put((x+" "+y), time);
+                    time++;
+                }
+            }else if(direction.equals("S")){
+                for(int i=0; i < steps;i++){
+                    y-=1;
+                    if(visited.containsKey(x+" "+y)){
+                        max = Math.min(max, time-visited.get(x+" "+y));
+                    }
+                    visited.put((x+" "+y), time);
+                    time++;
+                }
+            } else {
+                for(int i=0; i < steps;i++){
+                    x-=1;
+                    if(visited.containsKey(x+" "+y)){
+                        max = Math.min(max, time-visited.get(x+" "+y));
+                    }
+                    visited.put((x+" "+y), time);
+                    time++;
+                }
+            }
+            statement+=2;
+        }
+        if(max == Integer.MAX_VALUE){
+            System.out.println(-1);
+            pw.println(-1);
+        }else{
+            pw.println(max);
+        }
+        pw.close();
     }
 }
