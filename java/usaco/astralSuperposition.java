42f1891fef4b5042b7c74eb4910e784a3241f803
idksarah <sarah.p.ngai@gmail.com>
2025-01-25 14:47:59 -0800

    astral

--- java/usaco/astralSuperposition.java
+++ java/usaco/astralSuperposition.java
@@ -5,9 +5,7 @@ import java.util.*;
 // 12:18 pm 1/25/2025
 
 public class astralSuperposition {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+    public static void checkSky(BufferedReader br) throws IOException{
         String[] NAB = br.readLine().split(" ");
         int N = Integer.parseInt(NAB[0]);
         int right = Integer.parseInt(NAB[1]);
@@ -20,39 +18,62 @@ public class astralSuperposition {
             }
         }
         int starCount = 0;
-        Set<String> possiblyFake = new HashSet<>();
+        Set<String> realGray = new HashSet<>();
+        Set<String> translatedGray = new HashSet<>();
         System.out.println(Arrays.deepToString(sky));
-        //rememebr to tierateo ver this multiple times for the diff skies
-        for(int time=0; time < T; time++){
-            
-        }
         for(int a=0; a < N; a++){
             for(int b =0; b < N; b++){
                 if(sky[a][b] == 'G'){
-                    String coord = String.valueOf(b) + String.valueOf(a);
-                    if(!possiblyFake.contains(coord)){
-                        starCount++;
-                    } else {
+                    String coord = String.valueOf(a) + String.valueOf(b);
+                    String reverseTranslatedCoord = String.valueOf(a-down) + String.valueOf(b-right);
+                    String translatedCoord = String.valueOf(a+down) + String.valueOf(b+right);
+                    System.out.println("curren coord" + coord);
+                    if(!translatedGray.contains(coord)){
                         if((a-down) >= 0 && (b - right) >= 0){
-                            if(sky[a-down][b-right] == 'B' || sky[a-down][b-right] == 'G'){
+                            if(!realGray.contains(reverseTranslatedCoord)){ //check for reverse trnsalsted here
                                 starCount++;
-                                possiblyFake.add(coord);
+                                realGray.add(coord);
+                                translatedGray.add(translatedCoord);
                             }
-                        } else {
-                            System.out.println(-1);
+                            // else{
+                            //     System.out.println(-1);
+                            //     return;
+                            // }
                         }
-                    }
+                        realGray.add(coord);
+                        starCount++;
+                    } 
                 } else if(sky[a][b] =='B'){
                     if((a-down) >= 0 && (b - right) >= 0){
+                        System.out.println("possibly fake star2");
                         if(sky[a-down][b-right] == 'B' || sky[a-down][b-right] == 'G'){
-                            starCount++;
+                            System.out.println("real" + a + " "+b);
+                            realGray.add(String.valueOf(a+down) + String.valueOf(b+right));
                         }
                     } else {
-                        System.out.println(-1);
+                        starCount=-1;
+                        System.out.println(starCount);
+                        System.out.println("real" + a + " "+b);
+                        return;
                     }
                 }
             }
         }
+        for (String star : realGray) {
+            System.out.println("accepted stars" + star);
+        }
+        for (String star : translatedGray) {
+            System.out.println("translated stars" + star);
+        }
         System.out.println(starCount);
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        //rememebr to tierateo ver this multiple times for the diff skies
+        for(int time=0; time < T; time++){
+            System.out.println("time" + T);
+            checkSky(br);
+        }
     }
 }
