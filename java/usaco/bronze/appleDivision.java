b3407252c99cfae4b412b0a5eb52d33b8f803710
idksarah <sarah.p.ngai@gmail.com>
2025-02-04 21:20:36 -0800

    sleepinginClass

--- java/usaco/bronze/appleDivision.java
+++ java/usaco/bronze/appleDivision.java
@@ -1,3 +1,25 @@
+import java.io.*;
+import java.util.*;
+
 public class appleDivision {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] inputs = new int[N];
+        for(int i =0; i < N; i++){
+             inputs[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(inputs);
+        ArrayList<Integer> left = new ArrayList<>(); 
+        ArrayList<Integer> right = new ArrayList<>();
+
+        if(N%2 == 0){
+            for(int i = 0; i < N/2; i+=2){
+            }
     
+        }
+    }
 }
--- java/usaco/bronze/sleepingInClass.java
+++ java/usaco/bronze/sleepingInClass.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 
 // 5:32 pm 2/3/25
+// 8:31 pm
+// 9:09 sample
+// passes 10/11 idc
 
 public class sleepingInClass {    
     public static void main(String[] args) throws IOException{
@@ -10,7 +13,7 @@ public class sleepingInClass {
 
         int T = Integer.parseInt(st.nextToken());
 
-        for(int a = 0; a < T; a ++){ //each case
+        for(int a = 0; a < T; a ++){
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
@@ -19,44 +22,39 @@ public class sleepingInClass {
                 nums.add(Integer.parseInt(st.nextToken()));
             }
             int sum = nums.stream().mapToInt(Integer::intValue).sum();
-
-            ArrayList<Integer> sumFactors = new ArrayList<>();
-            for(int b = 1; b <= sum; b++){
-                if(sum % b == 0){
-                    sumFactors.add(b);
+            if(sum == 0 ){
+                System.out.println(0);
+            } else {
+                HashSet<Integer> sumFactors = new HashSet<>();
+                for(int b = 1; b <= sum/2; b++){
+                    if(sum % b == 0){
+                        sumFactors.add(b);
+                        sumFactors.add(sum/b);
+                    }
                 }
+                int count = Integer.MAX_VALUE;
+                for(Integer factor: sumFactors){
+                    int currDiff = factor;
+                    int tempCount = 0;
+                    for(int b = 0; b < nums.size(); b++){
+                        if(!nums.get(b).equals(factor)){
+                            currDiff -= nums.get(b);
+                            if(currDiff < 0){
+                                break;
+                            } else if(currDiff == 0 ){
+                                currDiff = factor;
+                                tempCount--;
+                            }
+                            tempCount++;
+                        }
+                    }
+                    if(currDiff > 0 ){
+                        count = Math.min(count, tempCount);
+                    }
+                }
+                System.out.println(count);
+                
             }
-            System.out.println(sum);
-            System.out.println(sumFactors);
-            int count = Integer.MAX_VALUE;
-        //     for(int b = 0; b < sumFactors.size(); b++){
-        //         int tempCount = 0;
-        //         for(Integer factor : sumFactors){
-        //             List<Integer> newNums = new ArrayList<>(nums);
-        //             int d = 0;
-        //             for(int c = 0; c < nums.size(); c ++){
-        //                 while(newNums.get(d) < b){
-        //                     System.out.println("d"+d);
-        //                     System.out.println("num" + newNums.get(d));
-        //                     if(newNums.get(d) > b){
-        //                         break;
-        //                     } else {
-        //                         newNums.set(d, newNums.get(d) + newNums.get(d+1));
-        //                         newNums.remove(d+1);
-        //                         System.out.println(newNums);
-        //                         if(nums.get(d) <= b){
-        //                             tempCount++;
-        //                             System.out.println(tempCount);
-        //                         }
-        //                     }
-        //                 }
-        //                 System.out.println("d++");
-        //                 d++;
-        //             }
-        //         count = Math.min(count, tempCount);
-        //     }
-        //     // System.out.println(count);
-        // }
+        }
     }
-}
 }
