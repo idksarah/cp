a2a0d1cc67ed8a9a873dcb47a5a9ef9641d3a383
idksarah <sarah.p.ngai@gmail.com>
2025-02-16 18:33:29 -0800

    guessTheAnimal

---
+++ java/usaco/bronze/guessTheAnimal.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+// thinking 5:35
+// 6:30
+// 6:37 pm 2/15/25
+// passes sample 6:30 2/16
+// 6:38 pm
+
+public class guessTheAnimal {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("guess.in"));
+        // BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter("guess.out");
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        HashSet<String>[] traits = new HashSet[N];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            String temp = st.nextToken(); // ignore animal
+            temp = st.nextToken(); // ignore number
+            traits[i] = new HashSet<>();
+            while(st.hasMoreTokens()){
+                traits[i].add(st.nextToken());
+            }
+        }
+        int max = 0;
+        for(int a = 0; a < N; a++){
+            ArrayList<String> curAnimalsTraits = new ArrayList<>(traits[a]);
+            for(int b = 0; b < N; b++){
+                int currCount = 0;
+                if (b != a){
+                    for(int c = 0; c < curAnimalsTraits.size(); c++){
+                        if(traits[b].contains(curAnimalsTraits.get(c))){
+                            currCount++;
+                        }
+                    }
+                }
+                max = Math.max(max, currCount);
+            }
+        }
+        pw.println(max+1);
+        pw.close();
+    }
+}
