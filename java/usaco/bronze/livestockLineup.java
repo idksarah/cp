c740425a579c01d57c35246fbdfaee0a439f45ac
idksarah <sarah.p.ngai@gmail.com>
2025-04-14 20:29:13 -0700

    permute

--- java/usaco/bronze/livestockLineup.java
+++ java/usaco/bronze/livestockLineup.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import javax.lang.model.element.Element;
 
 // 12:27 3/18/25
 // 4:57 pm 3/21
@@ -7,34 +8,57 @@ import java.util.*;
 
 public class livestockLineup {
     static String[][] constraints;
-    static ArrayList goodCows;
+    static ArrayList<String> alphaCows; 
     public static void main (String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        LinkedHashMap<String, Integer> alphaCowMap = new LinkedHashMap<>();
+        alphaCows = new ArrayList<String>(Arrays.asList("Beatrice", "Belinda", "Bella"));
+        // for (int i = 0; i < cows.length; i++) {
+        //     alphaCowMap.put(cows[i], i);
+        // }
 
-        String[] cows = {"Beatrice", "Belinda", "Bella", "Bessie", "Betsy", "Blue", "Buttercup", "Sue"};
-        for (int i = 0; i < cows.length; i++) {
-            alphaCowMap.put(cows[i], i);
-        }
-
-        LinkedHashMap<String, Integer> testCowMap = new LinkedHashMap<>();
         int N = Integer.parseInt(br.readLine());
+        constraints = new String[N][2];
         for(int i =0; i < N; i++){
             String[] instructions = br.readLine().split(" ");
             constraints[i][0] = instructions[0];
-            constraints[i][1] = instructions[1];
+            constraints[i][1] = instructions[5];
         }
-        // for (int i = 0; i < constraints.length; i++) {
-        //     System.out.println(Arrays.toString(constraints[i]));
-        // }
-        // ArrayList initial = new ArrayList<>();
-        // initial.add(0);
-        // recurse(initial);
-        // for(int i = 0; i < goodCows.size(); i++){
-        //     System.out.println(intToCows.get(goodCows.get(i)));
-        // }
-    
+        LinkedHashMap<String, Integer> blank = new LinkedHashMap<>();
+        recurse(blank, 0, alphaCows);
+    }
+    public static void recurse(LinkedHashMap<String, Integer> cows, Integer index, ArrayList<String> leftOverCows){
+        System.out.println(cows);
+        if (cows.size() == 8){
+            System.out.println("check");            
+            if (check(cows)){
+                for(String cow : cows.keySet()){
+                    System.out.println(cow);
+                }
+            }
+            System.exit(0); //sus
+        } else {
+            ArrayList<String> copy = new ArrayList<String>(leftOverCows);
+            for (String cow : copy) {
+                cows.put(cow, index);
+                leftOverCows.remove(cow);
+                recurse(cows, index + 1, leftOverCows);
+                leftOverCows.add(cow);
+                cows.remove(cow, index);
+            }
+            
+        }
+    }
+
+    public static boolean check (LinkedHashMap<String, Integer> cows){
+        boolean passes = true;
+        for (int a = 0; a < constraints.length; a++){
+            String cowA = constraints[a][0];
+            String cowB = constraints[a][1];
+            if ((cows.get(cowA) - 1 != cows.get(cowB)) && cows.get(cowA) + 1 != cows.get(cowB)){
+                passes = false;
+            }
+        }
+        return passes;
     }
 }
