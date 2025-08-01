2c92233aec8fccb277e8c6d737dad9d6d801c74f
idksarah <sarah.p.ngai@gmail.com>
2025-08-01 13:21:29 -0700

    idek

--- bronze/c++/sleepyCowHerding.cpp
+++ bronze/c++/sleepyCowHerding.cpp
@@ -1,34 +1,64 @@
 #include <iostream>
 #include <map>
 #include <algorithm>
+#include <iterator>
 using namespace std;
 
-map <int, int> cows;
-
-int main () {
-    // freopen("herding.in", "r", stdin);
-    // freopen("herding.out", "w", stdout);
-    int c[3];
-    scanf("%d %d %d", &c[0], &c[1], &c[2]);
-    sort(c, c+3);
-    
-    if (abs(cows[0] - cows[1]) == 1 && (abs(cows[1] - cows[2] == 1))){
-        printf("%n", 0);
-        printf("%n", 0);   
-    } else {
-        printf("%n", getMin());
-        printf("%n", getMax());
-    }
-    
-}
+int c[3];
+int d[3];
 
 int getMin() {
-    int gap = min(abs(cows[0] - cows[1]), abs(cows[1] - cows[2]));
-    while (gap != 0){
-
+    int count = 0;
+    int gap1 = c[1] - c[0];
+    int gap2 = c[2] - c[1];
+    while (gap1 > 1 || gap2 > 1){
+        int endpoint = c[1];
+        if (gap1 < gap2){
+            int middle = c[1] - 1;
+            c[2] = endpoint;
+            c[1] = middle;
+        } else {
+            int middle = c[1] + 1;
+            c[0] = endpoint;
+            c[1] = middle;
+        }
+        gap1 = c[1] - c[0];
+        gap2 = c[2] - c[1];
+        count++;
     }
+    return count;
 }
 
 int getMax() {
+    int count = 0;
+    int gap1 = d[1] - d[0];
+    int gap2 = d[2] - d[1];
+    while (gap1 > 1 || gap2 > 1){ 
+        int endpoint = d[1];
+        if (gap1 < gap2){
+            int middle = d[2] - 1;
+            d[0] = endpoint;
+            d[1] = middle;
+        } else {
+            int middle = d[0] + 1;
+            d[2] = endpoint;
+            d[1] = middle;
+        }
+        gap1 = d[1] - d[0];
+        gap2 = d[2] - d[1];
+        count++;
+    }
+    return count;
+}
 
+int main () {
+    freopen("herding.in", "r", stdin);
+    freopen("herding.out", "w", stdout);
+    scanf("%d %d %d", &c[0], &c[1], &c[2]);
+    sort(c, c+3);
+    copy(begin(c), end(c), begin(d));
+    
+    printf("%d\n", getMin());
+    printf("%d", getMax());
+    
 }
