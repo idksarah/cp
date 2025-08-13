0e9d21fe62cecdd67a551c79c55d5231843d0423
idksarah <sarah.p.ngai@gmail.com>
2025-08-12 20:57:16 -0700

    boyoing yoing yoing

--- bronze/c++/milkingOrder.cpp
+++ bronze/c++/milkingOrder.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <set>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 int main () {
@@ -9,30 +10,16 @@ int main () {
     // freopen("milkorder.out", "w", stdout);
     int n, m, k;
     scanf("%d %d %d", &n, &m, &k);
-    vector<int> hierarchy(m);
-    vector<vector<int>> guaranteed (k, vector<int>(2));
-    set<int> guaranteedIndexes;
-    for(int i = 0; i < m; i++){
-        scanf("%d", &hierarchy[i]);
+    vector<int> nums(n), hierarchy(m);
+    for(int i = 0; i < m; i++) scanf("%d", &hierarchy[i]);
+    for(int i = 0, index, value; i < k; i++){
+        scanf("%d %d", &index, &value);
+        nums[index - 1] = value;
     }
-    for(int i = 0; i < k; i++){
-        scanf("%d %d", &guaranteed[i][0], &guaranteed[i][1]);
-        guaranteedIndexes.insert(guaranteed[i][1]);
-    }
-    sort(guaranteed.begin(), guaranteed.end(), [](const vector<int>& a, const vector<int> &b) {
-        return a[1] < b[1];
-    });
     for(int i = 0; i < n; i++){
-        if(guaranteedIndexes.find(i) != guaranteedIndexes.end()){
-            int guaranteedLeft, guaranteedRight;
-            for(int j = 0; j < k; j++){
-                if(guaranteed[j][0] < i){
-                    guaranteedLeft++;
-                } else {
-                    guaranteedRight++;
-                }
-            }
-            int hierarchyLeft, hierarchyRight;
-        }   
+        if (nums[i] == 0){ // if blank
+
+        }
     }
+    
 }
