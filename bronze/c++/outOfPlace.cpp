5597e64ea5fb4af7278a3a5b01fb646caee6cd20
idksarah <sarah.p.ngai@gmail.com>
2025-08-26 18:51:25 -0700

    yoink

--- bronze/c++/outOfPlace.cpp
+++ bronze/c++/outOfPlace.cpp
@@ -1,37 +1,21 @@
 #include <vector>
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
 using namespace std;
 
 int main () {
-    // freopen("outofplace.in", "r", stdin);
-    // freopen("outofplace.out", "w", stdout);
+    freopen("outofplace.in", "r", stdin);
+    freopen("outofplace.out", "w", stdout);
     int n; cin >> n;
     vector<int> nums(n);
     for (int i = 0; i < n; i++) { cin >> nums[i]; }
-    int count = 0; int bessie = 0;
-    // for (int i = 0; i < n; i++) {
-    //     if (i < n && nums[i] > nums[i + 1]) { bessie = i;}
-    //     else if (i > 0 && nums[i] < nums[i - 1]) {
-    //         bessie = i;
-    //     } else if (i < n && i > 0 && (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) || (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])) {
-    //         bessie = i;
-    //     }
-    // }
-    for (int i = n - 1; i > 0; i--) {
-        // for(int i = 0; i < n; i++){
-        //     cout << nums[i] << " ";
-        // }
-        if (nums[i] < nums[i - 1]) {
+    int count = -1;
+    vector<int> sorted = nums;
+    sort(sorted.begin(), sorted.end());
+    for (int i = 0; i < n; i++) {
+        if (sorted[i] != nums[i]) {
             count++;
-            int below = nums[i - 1];
-            int above = nums[i];
-            int downBoy = 0;
-            while (nums[i - 1] == nums[i - 2 - downBoy]) {
-                downBoy++;
-            }
-            nums[i - 1 - downBoy] = above;
-            nums[i] = below;
         }
     }
     cout << count;
